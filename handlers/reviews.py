2495d4da5ce20529213b80383be7b37bac4d5dcb
fernout <romanabramovic174@gmail.com>
2024-12-18 23:59:40 +0600

    Telegrambot

--- handlers/reviews.py
+++ handlers/reviews.py
@@ -4,60 +4,67 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
 
-opros_router = Router()
+questions_router = Router()
 
-class opros(StatesGroup):
+class Questions(StatesGroup):
     name = State()
     phone_number = State()
     food_rating = State()
     cleanliness_rating = State()
     extra_comments = State()
 
-@opros_router.message(Command("opros"))
-async def start_opros(message: types.Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await message.answer("Как вас зовут?")
-    await state.set_state(opros.name)
-
-@opros_router.message(opros.name)
+@questions_router.message(Questions.name)
 async def phone_number(message: types.Message, state: FSMContext):
     await message.answer("Как с вами можно связаться?")
-    await state.set_state(opros.phone_number)
+    await state.set_state(Questions.phone_number)
 
-@opros_router.message(opros.phone_number)
+@questions_router.message(Questions.phone_number)
 async def food_rating(message: types.Message, state: FSMContext):
+    await state.update_data(phone_number=message.text)
     await message.answer("Какую оценку поставите нашей пиццерии? от 1 до 10")
-    await state.set_state(opros.food_rating)
+    await state.set_state(Questions.food_rating)
 
-@opros_router.message(opros.food_rating)
+@questions_router.message(Questions.food_rating)
 async def cleanliness_rating(message: types.Message, state: FSMContext):
+    await state.update_data(food_rating=message.text)
     food_rating = message.text
     if not food_rating.isdigit():
         await message.answer("Введите число от 1 до 10")
         return
     food_rating = int(food_rating)
     if food_rating < 1 or food_rating > 10:
-        return
-    await message.answer("Как оцениваете чистоту заведения? от 1 до 10")
-    await state.set_state(opros.cleanliness_rating)
+        if not cleanliness_rating.isdigit():
+            await message.answer("Введите число только от 1 до 10.")
+            return
+        if food_rating < 1 or food_rating > 10:
+            await message.answer("Пожалуйста, введите число от 1 до 10.")
+            return
 
-@opros_router.message(opros.cleanliness_rating)
+    await message.answer("Как оцениваете чистоту заведения? от 1 до 10")
+    await state.set_state(Questions.cleanliness_rating)
+
+@questions_router.message(Questions.cleanliness_rating)
 async def extra_comments(message: types.Message, state: FSMContext):
+    await state.update_data(extra_comments=message.text)
     cleanliness_rating = message.text
     if not cleanliness_rating.isdigit():
         await message.answer("Введите число от 1 до 10")
         return
     cleanliness_rating = int(cleanliness_rating)
     if cleanliness_rating < 1 or cleanliness_rating > 10:
-        return
+        if not cleanliness_rating.isdigit():
+            await message.answer("Введите число только от 1 до 10.")
+            return
+        if cleanliness_rating < 1 or cleanliness_rating > 10:
+            await message.answer("Пожалуйста, введите число от 1 до 10.")
+            return
 
     await message.answer("Дополнительные комментарии/жалоба?")
-    await state.set_state(opros.extra_comments)
+    await state.set_state(Questions.extra_comments)
 
-@opros_router.message(opros.cleanliness_rating)
-
-async def comments(message: types.Message, state: FSMContext):
-    await state.update_data(comments=message.text)
+@questions_router.message(Questions.cleanliness_rating)
+async def extra_comments(message: types.Message, state: FSMContext):
+    await state.update_data(extra_comments=message.text)
     data = await state.get_data()
     await message.answer(f"Спасибо за ваш отзыв, {data['name']}!\n"
                          f"Способ связи: {data['phone_number']}\n"
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from bot_config import dp, bot
 from handlers.start import start_router
 from handlers.picture import picture_router
 from handlers.other_messages import other_echo_handler
-from handlers.reviews import opros_router
+from handlers.reviews import questions_router
 
 async def main():
     dp.include_router(start_router)
     dp.include_router(picture_router)
     dp.include_router(other_echo_handler)
-    dp.include_router(opros_router)
+    dp.include_router(questions_router)
     await dp.start_polling(bot)
 
 
